1660464c00d075175f7a54b0d86730c0a9b86f52
Joe-Tech <josephodney@gmail.com>
2015-12-13 07:10:08 -0500

    Bunch of code cleaned up

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -13,9 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.android.settings;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -24,6 +22,7 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.os.SELinux;
 import android.os.SystemClock;
 import android.os.SystemProperties;
@@ -34,16 +33,15 @@ import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
+import android.telephony.CarrierConfigManager;
 import android.text.TextUtils;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.widget.Toast;
-
 import com.android.internal.logging.MetricsLogger;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Index;
 import com.android.settings.search.Indexable;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -56,50 +54,43 @@ import java.util.List;
 import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 public class DeviceInfoSettings extends SettingsPreferenceFragment implements Indexable {
-
     private static final String LOG_TAG = "DeviceInfoSettings";
     private static final String FILENAME_PROC_VERSION = "/proc/version";
     private static final String FILENAME_MSV = "/sys/board_properties/soc/msv";
-
     private static final String KEY_MANUAL = "manual";
     private static final String KEY_REGULATORY_INFO = "regulatory_info";
-    private static final String KEY_MOD_COLDFUSION_DATE = "coldfusion_date";
+    private static final String KEY_SYSTEM_UPDATE_SETTINGS = "system_update_settings";
+    private static final String PROPERTY_URL_SAFETYLEGAL = "ro.url.safetylegal";
     private static final String PROPERTY_SELINUX_STATUS = "ro.build.selinux";
     private static final String KEY_KERNEL_VERSION = "kernel_version";
-    private static final String KEY_BUILD_NUMBER = "build_number";
+    private static final String KEY_MOD_COLDFUSION_DATE = "coldfusion_date";
     private static final String KEY_DEVICE_MODEL = "device_model";
     private static final String KEY_SELINUX_STATUS = "selinux_status";
     private static final String KEY_BASEBAND_VERSION = "baseband_version";
     private static final String KEY_FIRMWARE_VERSION = "firmware_version";
     private static final String KEY_SECURITY_PATCH = "security_patch";
+    private static final String KEY_UPDATE_SETTING = "additional_system_update_settings";
     private static final String KEY_EQUIPMENT_ID = "fcc_equipment_id";
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
-
+    private static final String KEY_SAFETY_LEGAL = "safetylegal";
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
-
     long[] mHits = new long[3];
     int mDevHitCountdown;
     Toast mDevHitToast;
-
     @Override
     protected int getMetricsCategory() {
         return MetricsLogger.DEVICEINFO;
     }
-
     @Override
     protected int getHelpResource() {
         return R.string.help_uri_about;
     }
-
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-
         addPreferencesFromResource(R.xml.device_info_settings);
-
         setStringSummary(KEY_FIRMWARE_VERSION, Build.VERSION.RELEASE);
         findPreference(KEY_FIRMWARE_VERSION).setEnabled(true);
         String patch = Build.VERSION.SECURITY_PATCH;
@@ -115,7 +106,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
             setStringSummary(KEY_SECURITY_PATCH, patch);
         } else {
             getPreferenceScreen().removePreference(findPreference(KEY_SECURITY_PATCH));
-
         }
         setValueSummary(KEY_BASEBAND_VERSION, "gsm.version.baseband");
         setStringSummary(KEY_DEVICE_MODEL, Build.MODEL + getMsvSuffix());
@@ -133,30 +123,43 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
             String status = getResources().getString(R.string.selinux_status_permissive);
             setStringSummary(KEY_SELINUX_STATUS, status);
         }
-
         // Remove selinux information if property is not present
         removePreferenceIfPropertyMissing(getPreferenceScreen(), KEY_SELINUX_STATUS,
                 PROPERTY_SELINUX_STATUS);
-
+        // Remove Safety information preference if PROPERTY_URL_SAFETYLEGAL is not set
+        removePreferenceIfPropertyMissing(getPreferenceScreen(), KEY_SAFETY_LEGAL,
+                PROPERTY_URL_SAFETYLEGAL);
         // Remove Equipment id preference if FCC ID is not set by RIL
         removePreferenceIfPropertyMissing(getPreferenceScreen(), KEY_EQUIPMENT_ID,
                 PROPERTY_EQUIPMENT_ID);
-
         // Remove Baseband version if wifi-only device
         if (Utils.isWifiOnly(getActivity())) {
             getPreferenceScreen().removePreference(findPreference(KEY_BASEBAND_VERSION));
         }
-
         // Dont show feedback option if there is no reporter.
         if (TextUtils.isEmpty(getFeedbackReporterPackage(getActivity()))) {
             getPreferenceScreen().removePreference(findPreference(KEY_DEVICE_FEEDBACK));
         }
-
         /*
          * Settings is a generic app and should not contain any device-specific
          * info.
          */
-
+        final Activity act = getActivity();
+        // These are contained by the root preference screen
+        PreferenceGroup parentPreference = getPreferenceScreen();
+        if (UserHandle.myUserId() == UserHandle.USER_OWNER) {
+            Utils.updatePreferenceToSpecificActivityOrRemove(act, parentPreference,
+                    KEY_SYSTEM_UPDATE_SETTINGS,
+                    Utils.UPDATE_PREFERENCE_FLAG_SET_TITLE_TO_MATCHING_ACTIVITY);
+        } else {
+            // Remove for secondary users
+            removePreference(KEY_SYSTEM_UPDATE_SETTINGS);
+        }
+        // Read platform settings for additional system update setting
+        removePreferenceIfBoolFalse(KEY_UPDATE_SETTING,
+                R.bool.config_additional_system_update_setting_enable);
+        // Remove manual entry if none present.
+        removePreferenceIfBoolFalse(KEY_MANUAL, R.bool.config_show_manual);
         // Remove regulatory information if none present.
         final Intent intent = new Intent(Settings.ACTION_SHOW_REGULATORY_INFO);
         if (getPackageManager().queryIntentActivities(intent, 0).isEmpty()) {
@@ -166,7 +169,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
             }
         }
     }
-
     @Override
     public void onResume() {
         super.onResume();
@@ -175,7 +177,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
                         android.os.Build.TYPE.equals("eng")) ? -1 : TAPS_TO_BE_A_DEVELOPER;
         mDevHitToast = null;
     }
-
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         if (preference.getKey().equals(KEY_FIRMWARE_VERSION)) {
@@ -187,7 +188,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
                     Log.d(LOG_TAG, "Sorry, no fun for you!");
                     return false;
                 }
-
                 Intent intent = new Intent(Intent.ACTION_MAIN);
                 intent.setClassName("android",
                         com.android.internal.app.PlatLogoActivity.class.getName());
@@ -200,10 +200,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         } else if (preference.getKey().equals(KEY_BUILD_NUMBER)) {
             // Don't enable developer options for secondary users.
             if (UserHandle.myUserId() != UserHandle.USER_OWNER) return true;
-
             final UserManager um = (UserManager) getSystemService(Context.USER_SERVICE);
             if (um.hasUserRestriction(UserManager.DISALLOW_DEBUGGING_FEATURES)) return true;
-
             if (mDevHitCountdown > 0) {
                 mDevHitCountdown--;
                 if (mDevHitCountdown == 0) {
@@ -220,7 +218,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
                     Index.getInstance(
                             getActivity().getApplicationContext()).updateFromClassNameResource(
                                     DevelopmentSettings.class.getName(), true, true);
-
                 } else if (mDevHitCountdown > 0
                         && mDevHitCountdown < (TAPS_TO_BE_A_DEVELOPER-2)) {
                     if (mDevHitToast != null) {
@@ -241,10 +238,36 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
             }
         } else if (preference.getKey().equals(KEY_DEVICE_FEEDBACK)) {
             sendFeedback();
+        } else if(preference.getKey().equals(KEY_SYSTEM_UPDATE_SETTINGS)) {
+            CarrierConfigManager configManager =
+                    (CarrierConfigManager) getSystemService(Context.CARRIER_CONFIG_SERVICE);
+            PersistableBundle b = configManager.getConfig();
+            if (b.getBoolean(CarrierConfigManager.KEY_CI_ACTION_ON_SYS_UPDATE_BOOL)) {
+                ciActionOnSysUpdate(b);
+            }
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
-
+    /**
+     * Trigger client initiated action (send intent) on system update
+     */
+    private void ciActionOnSysUpdate(PersistableBundle b) {
+        String intentStr = b.getString(CarrierConfigManager.
+                KEY_CI_ACTION_ON_SYS_UPDATE_INTENT_STRING);
+        if (!TextUtils.isEmpty(intentStr)) {
+            String extra = b.getString(CarrierConfigManager.
+                    KEY_CI_ACTION_ON_SYS_UPDATE_EXTRA_STRING);
+            String extraVal = b.getString(CarrierConfigManager.
+                    KEY_CI_ACTION_ON_SYS_UPDATE_EXTRA_VAL_STRING);
+            Intent intent = new Intent(intentStr);
+            if (!TextUtils.isEmpty(extra)) {
+                intent.putExtra(extra, extraVal);
+            }
+            Log.d(LOG_TAG, "ciActionOnSysUpdate: broadcasting intent " + intentStr +
+                    " with extra " + extra + ", " + extraVal);
+            getActivity().getApplicationContext().sendBroadcast(intent);
+        }
+    }
     private void removePreferenceIfPropertyMissing(PreferenceGroup preferenceGroup,
             String preference, String property ) {
         if (SystemProperties.get(property).equals("")) {
@@ -257,7 +280,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
             }
         }
     }
-
     private void removePreferenceIfBoolFalse(String preference, int resId) {
         if (!getResources().getBoolean(resId)) {
             Preference pref = findPreference(preference);
@@ -266,7 +288,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
             }
         }
     }
-
     private void setStringSummary(String preference, String value) {
         try {
             findPreference(preference).setSummary(value);
@@ -275,7 +296,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
                 getResources().getString(R.string.device_info_default));
         }
     }
-
     private void setValueSummary(String preference, String property) {
         try {
             findPreference(preference).setSummary(
@@ -285,7 +305,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
             // No recovery
         }
     }
-
     private void sendFeedback() {
         String reporterPackage = getFeedbackReporterPackage(getActivity());
         if (TextUtils.isEmpty(reporterPackage)) {
@@ -295,7 +314,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         intent.setPackage(reporterPackage);
         startActivityForResult(intent, 0);
     }
-
     /**
      * Reads a line from the specified file.
      * @param filename the file to read from
@@ -310,26 +328,21 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
             reader.close();
         }
     }
-
     public static String getFormattedKernelVersion() {
         try {
             return formatKernelVersion(readLine(FILENAME_PROC_VERSION));
-
         } catch (IOException e) {
             Log.e(LOG_TAG,
                 "IO Exception when getting kernel version for Device Info screen",
                 e);
-
             return "Unavailable";
         }
     }
-
     public static String formatKernelVersion(String rawKernelVersion) {
         // Example (see tests for more):
         // Linux version 3.0.31-g6fb96c9 (android-build@xxx.xxx.xxx.xxx.com) \
         //     (gcc version 4.6.x-xxx 20120106 (prerelease) (GCC) ) #1 SMP PREEMPT \
         //     Thu Jun 28 11:02:39 PDT 2012
-
         final String PROC_VERSION_REGEX =
             "Linux version (\\S+) " + /* group 1: "3.0.31-g6fb96c9" */
             "\\((\\S+?)\\) " +        /* group 2: "x@y.com" (kernel builder) */
@@ -337,7 +350,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
             "(#\\d+) " +              /* group 3: "#1" */
             "(?:.*?)?" +              /* ignore: optional SMP, PREEMPT, and any CONFIG_FLAGS */
             "((Sun|Mon|Tue|Wed|Thu|Fri|Sat).+)"; /* group 4: "Thu Jun 28 11:02:39 PDT 2012" */
-
         Matcher m = Pattern.compile(PROC_VERSION_REGEX).matcher(rawKernelVersion);
         if (!m.matches()) {
             Log.e(LOG_TAG, "Regex did not match on /proc/version: " + rawKernelVersion);
@@ -351,7 +363,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
             m.group(2) + " " + m.group(3) + "\n" + // x@y.com #1
             m.group(4);                            // Thu Jun 28 11:02:39 PDT 2012
     }
-
     /**
      * Returns " (ENGINEERING)" if the msv file has a zero value, else returns "".
      * @return a string to append to the model number description.
@@ -372,7 +383,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         }
         return "";
     }
-
     private static String getFeedbackReporterPackage(Context context) {
         final String feedbackReporter =
                 context.getResources().getString(R.string.oem_preferred_feedback_reporter);
@@ -383,7 +393,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         // Additional checks to ensure the reporter is on system image, and reporter is
         // configured to listen to the intent. Otherwise, dont show the "send feedback" option.
         final Intent intent = new Intent(Intent.ACTION_BUG_REPORT);
-
         PackageManager pm = context.getPackageManager();
         List<ResolveInfo> resolvedPackages =
                 pm.queryIntentActivities(intent, PackageManager.GET_RESOLVED_FILTER);
@@ -407,13 +416,11 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         }
         return null;
     }
-
     /**
      * For Search.
      */
     public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
         new BaseSearchIndexProvider() {
-
             @Override
             public List<SearchIndexableResource> getXmlResourcesToIndex(
                     Context context, boolean enabled) {
@@ -421,12 +428,14 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
                 sir.xmlResId = R.xml.device_info_settings;
                 return Arrays.asList(sir);
             }
-
             @Override
             public List<String> getNonIndexableKeys(Context context) {
                 final List<String> keys = new ArrayList<String>();
                 if (isPropertyMissing(PROPERTY_SELINUX_STATUS)) {
                     keys.add(KEY_SELINUX_STATUS);
+                }
+                if (isPropertyMissing(PROPERTY_URL_SAFETYLEGAL)) {
+                    keys.add(KEY_SAFETY_LEGAL);
                 }
                 if (isPropertyMissing(PROPERTY_EQUIPMENT_ID)) {
                     keys.add(KEY_EQUIPMENT_ID);
@@ -439,13 +448,18 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
                 if (TextUtils.isEmpty(getFeedbackReporterPackage(context))) {
                     keys.add(KEY_DEVICE_FEEDBACK);
                 }
+                if (UserHandle.myUserId() != UserHandle.USER_OWNER) {
+                    keys.add(KEY_SYSTEM_UPDATE_SETTINGS);
+                }
+                if (!context.getResources().getBoolean(
+                        R.bool.config_additional_system_update_setting_enable)) {
+                    keys.add(KEY_UPDATE_SETTING);
+                }
                 return keys;
             }
-
             private boolean isPropertyMissing(String property) {
                 return SystemProperties.get(property).equals("");
             }
         };
-
 }
 
--- src/com/android/settings/coldfusion/PowerMenuActions.java
+++ src/com/android/settings/coldfusion/PowerMenuActions.java
@@ -31,5 +31,6 @@ public class PowerMenuActions extends SettingsPreferenceFragment implements
 
     @Override
     protected int getMetricsCategory() {
-        return MetricsLogger.APPLICATION;
+        return MetricsLogger.DISPLAY;
     }
+}
